6a579c0fe1df819a8ec5f01b1ebc3e0b8bc63638
dio_ <ae7006@gmail.com>
2025-06-07 02:31:00 +0900

    feat. TMP 추가

---
+++ Assets/Features/Binding/Scripts/IBindable.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Modules.Binding.Scripts
+{
+
+    public interface IBindable
+    {
+        public event Action<IBindable> OnValueChanged;
+        public IBindable Value { get; }
+        public void SetValue(IBindable bindable);
+    }
+  
+    public interface IBindable<T> 
+    {
+        public event Action<T> OnValueChanged;
+        public T Value { get; }
+        public void SetValue(T value);
+    }
+}
